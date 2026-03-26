ddc3865f2cab2dab9f88f0b035d2e74dc4df8e58
Stefano Di Masi <ste.dim@live.com>
2026-03-26 21:39:24 +0100

    Deploy pulito della build web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1649962757" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "325159963" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
